b6e8ea4aadee95b7c0d5012550920a8652b52c31
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-03-22 22:33:13 -0300

    Finalizando projeto01 sec03

--- sec04 - proj01/app.js
+++ sec04 - proj01/app.js
@@ -4,6 +4,7 @@ new Vue({
         lifeJogador: 100,
         lifeMonstro: 100,
         iniciou: false,
+        registros: [],
     },
     computed:{
         temResultado(){
@@ -15,6 +16,7 @@ new Vue({
             this.iniciou = true;
             this.lifeJogador = 100;
             this.lifeMonstro = 100;
+            this.registros = [];
         },
         desistirJogo(){
             this.iniciou = false;
@@ -31,10 +33,12 @@ new Vue({
             
             if(especial){
                 this.lifeJogador -= 5
-                this.lifeMonstro -= valor + 5 
+                this.lifeMonstro -= valor + 5
+                this.registraAtaque(5,valor+5) 
             }else{
                 this.lifeJogador -= valor + 5
                 this.lifeMonstro -= 5
+                this.registraAtaque(valor + 5, 5)
             }
             this.zeroLife()
         },
@@ -44,12 +48,20 @@ new Vue({
             this.lifeJogador -= valor
             this.lifeJogador += 5
             
+            this.registraCura( 5, valor)
+            
             this.zeroLife()
+        },
+        registraAtaque( ataqueJogador, ataqueMonstro){
+            this.registros.push({'ataqueJogador':ataqueJogador,'ataqueMonstro':ataqueMonstro})
+        },
+        registraCura(cura,ataqueMonstro){
+            this.registros.push({'cura': cura,'ataqueMonstro': ataqueMonstro})
         }
     },
     watch:{
         temResultado(value){
-            if(value) this.iniciou = false; 
+            if(value) this.iniciou = false;
         }   
     }
 
