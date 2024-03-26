e3e7b59d36e206b57faf208933cd497b6ef9ee2f
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-03-26 09:08:24 -0300

    feat: criados os componentes do desafio proposto

---
+++ sec07 - Componentes/Desafio-Sec07/componente-desafio/src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-footer class="pa-3">
+        <v-spacer></v-spacer>
+        <div>Curso Vue &copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>   
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+</style>
--- sec07 - Componentes/Desafio-Sec07/componente-desafio/src/main.js
+++ sec07 - Componentes/Desafio-Sec07/componente-desafio/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import Footer from './components/Footer.vue'
+import Main from './components/Main.vue'
+import Bar from './components/Bar.vue'
+import Navigation from './components/Navigation.vue'
 
 Vue.config.productionTip = false
+Vue.component('app-footer', Footer)
+Vue.component('app-main', Main)
+Vue.component('app-bar', Bar)
+Vue.component('app-navigation', Navigation)
 
 new Vue({
   vuetify,
