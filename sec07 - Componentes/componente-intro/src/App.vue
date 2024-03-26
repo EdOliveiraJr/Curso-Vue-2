149b7ff13b07952bbdf2b7866684bd2d87e6c344
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-03-26 08:19:28 -0300

    update: sec07 - Componentes; Incluindo inicio do Desafio

---
+++ sec07 - Componentes/componente-intro/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>Contador</h1>
+    <app-contadores/>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+  export default {
+    
+  }
+</script>
+
+<style></style>
---
+++ sec07 - Componentes/componente-intro/src/Contador.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="contador">
+        <span>{{ contador }}</span>
+        <button @click="adicionar">+</button>
+        <button @click="subtrair">-</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            contador: 0
+        }
+    },
+    methods: {
+        adicionar(){
+            this.contador++
+        },
+        subtrair(){
+            this.contador--
+        }
+    }
+}
+</script>
+
+<style  scoped>
+    .contador {
+        display: flex;
+        align-items: center;
+        justify-items: center;
+    }
+    button {
+        height: 30px;
+        width: 30px;
+        border-radius: 15px;
+        margin: 5px;
+        background-color: lightgreen;
+        border: none;
+    }
+
+    span {
+        font-size: xx-large;
+        height: 30px;
+        width: 30px;
+        border-radius: 15px;
+        margin: 5px;
+        border: none;
+    }
+</style>
