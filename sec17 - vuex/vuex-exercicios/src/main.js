05ed541aea38f399436fca08542c9013e8ee912e
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-03-31 19:03:54 -0300

    conclusão sec17

---
+++ sec17 - vuex/vuex-exercicios/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store/store'
+
+import Painel from './components/Painel'
+
+Vue.config.productionTip = false
+
+Vue.component('Painel', Painel)
+
+Vue.filter('dinheiro', valor => {
+	return `R$ ${parseFloat(valor).toFixed(2)}`.replace('.', ',')
+})
+
+new Vue({
+	store,
+	render: h => h(App),
+}).$mount('#app')
