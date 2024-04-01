7fc6f401be73279b9073fe33378ea797e5293359
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-04-01 11:17:15 -0300

    começo sec18

---
+++ sec18 - stock trader/stock-trader/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+   <v-toolbar app>
+        <v-toolbar-title class="headline text-uppercase mr-4">
+            <span>Stock</span>
+            <span class="font-weight-light">Trader</span>
+        </v-toolbar-title>
+        
+        <v-toolbar-items>
+            <v-btn flat to="/">Início</v-btn>
+            <v-btn flat to="/portfolio">Portfólio</v-btn>
+            <v-btn flat to="/stocks">Ações</v-btn>
+        </v-toolbar-items>
+        
+        <v-sapacer></v-sapacer>
+        
+        <v-toolbar-items>
+
+        </v-toolbar-items>
+        
+   </v-toolbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
