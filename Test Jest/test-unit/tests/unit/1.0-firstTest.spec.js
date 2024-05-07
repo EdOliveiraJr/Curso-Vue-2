76d6c5e34644b4cc14ce257b4c5b788029c44998
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-05-07 10:02:40 -0300

    fix: rename first test

---
+++ Test Jest/test-unit/tests/unit/1.0-firstTest.spec.js
@@ -0,0 +1,11 @@
+import { mount } from "@vue/test-utils";
+
+const App = {
+  template:`<div>Hello</div>`
+}
+
+test('App', () =>{
+  const wrapper = mount(App)
+  expect(wrapper.html()).toBe('<div>Hello</div>')
+
+})
