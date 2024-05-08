8073596e245e206a00dec440f5a140db5335563a
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-05-08 12:02:39 -0300

    feat: implements test using mock of the HTTP call

---
+++ Test Jest/test-unit/tests/unit/1.11-mockingHttpCalls/1.11-mockingHttpCalls.spec.js
@@ -0,0 +1,58 @@
+import { mount } from "@vue/test-utils";
+import App from "./App.vue";
+import { createStore } from "vuex";
+
+const createVuexStore = ()=> {
+  return createStore ({
+    state() {
+      return {
+        count: 0
+      }
+    },
+    mutations: {
+      increment(state) {
+        state.count++
+      }
+    }
+  })
+}
+
+function factory() {
+  const store = createVuexStore()
+  return mount(App, {
+    global: {
+      plugins: [store],
+    }
+  })
+}
+
+let mockGet = jest.fn()
+
+jest.mock('axios', () => ({
+  get: () => mockGet()
+}))
+
+describe('App', () =>{
+  beforeEach(() => {
+    mockGet = jest.fn()
+  })
+
+  it('render count when even', async ()=>{
+    const wrapper = factory()
+    await wrapper.find('button').trigger('click')
+    await wrapper.find('button').trigger('click')
+    expect(wrapper.html()).toContain('Count: 2. Count is even.')
+  })
+
+  it('render count when odd', async ()=>{
+    const wrapper = factory()
+    await wrapper.find('button').trigger('click')
+    expect(wrapper.html()).toContain('Count: 1. Count is odd.')
+  })
+
+  it('makes an api call', async () => {
+    const wrapper = factory()
+    await wrapper.find('button').trigger('click')
+    expect(mockGet).toHaveBeenCalledTimes(1)
+  })
+})
---
+++ Test Jest/test-unit/tests/unit/1.11-mockingHttpCalls/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <button @click="increment"> + </button>
+    <div v-if="count % 2 === 0">
+      Count: {{ count }}. Count is even.
+    </div>
+    <div v-if="count % 2 !== 0">
+      Count: {{ count }}. Count is odd.
+    </div>
+    <Fetcher/>
+</template>
+
+<script>
+import Fetcher from './Fetcher.vue';
+
+export default {
+  components:{ Fetcher },
+  computed: {
+    count(){
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    increment(){
+      this.$store.commit('increment')
+    }
+  },
+}
+</script>
