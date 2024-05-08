6f24d535d7c9bdb7c28afb51c3cd4f90d0cadd19
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-05-08 10:26:07 -0300

    feat: implements test using mock of the Vue Router

---
+++ Test Jest/test-unit/tests/unit/1.10-mockingVueRouther/1.10-mockingVueRouther.spec.js
@@ -0,0 +1,55 @@
+import { mount } from "@vue/test-utils";
+import App from "./App.vue";
+import { createStore } from "vuex";
+
+const createVuexStore = ()=> {
+  return createStore ({
+    state() {
+      return {
+        count: 0
+      }
+    },
+    mutations: {
+      increment(state) {
+        state.count++
+      }
+    }
+  })
+}
+
+function factory() {
+  const store = createVuexStore()
+  return mount(App, {
+    global: {
+      plugins: [store],
+      mocks: {
+        $route: {
+          params: {
+            postId: '1'
+          }
+        }
+      }
+    }
+  })
+}
+
+describe('App', () =>{
+  it('render count when even', async ()=>{
+    const wrapper = factory()
+    await wrapper.find('button').trigger('click')
+    await wrapper.find('button').trigger('click')
+    expect(wrapper.html()).toContain('Count: 2. Count is even.')
+  })
+
+  it('render count when odd', async ()=>{
+    const wrapper = factory()
+    await wrapper.find('button').trigger('click')
+    expect(wrapper.html()).toContain('Count: 1. Count is odd.')
+  })
+
+  it('render PostID', async ()=>{
+    const wrapper = factory()
+    expect(wrapper.html()).toContain('PostID: 1.')
+  })
+})
+1
---
+++ Test Jest/test-unit/tests/unit/1.10-mockingVueRouther/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <button @click="increment"> + </button>
+    <div v-if="count % 2 === 0">
+      Count: {{ count }}. Count is even.
+    </div>
+    <div v-if="count % 2 !== 0">
+      Count: {{ count }}. Count is odd.
+    </div>
+    <div>PostID: {{ postId }}.</div>
+</template>
+
+<script>
+
+export default {
+  computed: {
+    postId(){
+      return this.$route.params.postId
+    },
+    count(){
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    increment(){
+      this.$store.commit('increment')
+    }
+  },
+}
+</script>
